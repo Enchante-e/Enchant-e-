2cd1b23766218b449818b08ae73d4b97b4e7d3e3
the-other-clara <clara.lautre@edu.gobelins.fr>
2022-05-13 23:26:06 +0200

    Player for ambient music with play pause and updating objects

--- src/homepage/home.js
+++ src/homepage/home.js
@@ -2,7 +2,17 @@
 let logo = [...document.getElementsByClassName("logo")]
 let homeDiv = [...document.getElementsByClassName("homepage")]
 let musicBttn = [...document.getElementsByClassName("musicBttn")]
+let ambientPlayerHTML = document.getElementById("ambientPlayer")
+ambientPlayerHTML.volume = 0.4
+ambientPlayerHTML.play()
 
+musicBttn[0].addEventListener("click", () => {
+    if (ambientPlayerHTML.paused) {
+        ambientPlayerHTML.play()
+    } else {
+        ambientPlayerHTML.pause()
+    }
+})
 
 export const initHome = () => {
     document.body.classList.add("homeStyle")
@@ -13,3 +23,4 @@ export const closeHome = () => {
     logo[0].classList.remove("hidden")
     musicBttn[0].classList.remove("whiteTint")
 }
+
